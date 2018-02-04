d7c7a7720165143d03e68a0ca1f431be1892cdf5
Andreas Ek <andreas@elseif.se>
2018-02-04 17:05:38 +0100

    Shard theme started

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# videowebb
+# Videowebb - Strukturerad WordPressutveckling
 
 02 - Githubrepo
 
@@ -6,3 +6,6 @@
 * [GitFlow Examples](https://gitversion.readthedocs.io/en/latest/git-branching-strategies/gitflow-examples/)
 * [Tutorial](https://www.atlassian.com/git/tutorials/comparing-workflows/gitflow-workflow)
 * [Cheatsheet](https://danielkummer.github.io/git-flow-cheatsheet/)
+
+## Theme
+Based on [Shards](https://designrevision.com/demo/shards/)
--- web/app/themes/videowebb/index.php
+++ web/app/themes/videowebb/index.php
@@ -1,1 +1,29 @@
-Videowebb
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title><?php wp_title(); ?></title>
+
+    <?php wp_head(); ?>
+
+    <!-- CSS Dependencies -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/shards.min.css">
+  </head>
+  <body>
+
+    <h1><?=get_bloginfo('title')?></h1>
+
+
+    <?php wp_footer() ?>
+
+    <!-- Optional JavaScript -->
+    <!-- JavaScript Dependencies: jQuery, Popper.js, Bootstrap JS, Shards JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <script src="js/shards.js"></script>
+  </body>
+</html>
