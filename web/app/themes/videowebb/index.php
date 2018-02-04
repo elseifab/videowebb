7dc4287fb9f98f58b8fb165327d851ef6ab32dd4
Andreas Ek <andreas@elseif.se>
2018-02-04 17:26:59 +0100

    Shards started in theme

--- web/app/themes/videowebb/index.php
+++ web/app/themes/videowebb/index.php
@@ -12,8 +12,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
     <link rel="stylesheet" href="css/shards.min.css">
   </head>
-  <body>
-
+  <body <?=body_class()?>>
     <h1><?=get_bloginfo('title')?></h1>
 
 
